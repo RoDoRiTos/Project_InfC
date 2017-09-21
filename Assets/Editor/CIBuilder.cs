55fa58ef6fb9d61e5bd86981a834cf15588191e6
RoDoRiTos || !Un1qu3 <rodoritos_17@i.softbank.jp>
2017-09-22 05:55:18 +0900

    Initial Commit

---
+++ Assets/Editor/CIBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+class CIBuilder {
+    static string[] scenes = FindEnabledEditorScenes();
+
+    static string appname = "Infinite Collosseum";
+    static string targetPath = "AutoBuild";
+
+    private static string[] FindEnabledEditorScenes() {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void PerformPCBuild() {
+        string target = targetPath;
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.targetGroup = BuildTargetGroup.Standalone;
+        options.target = BuildTarget.StandaloneWindows;
+        options.locationPathName = targetPath + "/" + appname + ".exe";
+        options.options = BuildOptions.None;
+        Build(options);
+    }
+
+
+    static void Build(BuildPlayerOptions options) {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(options.targetGroup, options.target);
+        string res = BuildPipeline.BuildPlayer(options);
+        if (res.Length > 0) {
+            throw new Exception("BuildPlayer failure: " + res);
+        }
+    }
+}
